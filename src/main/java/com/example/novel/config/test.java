9f6d6b3f2d05a1bb0550d54dc8730e1dc2227d83
zszz112 <108114968+zszz112@users.noreply.github.com>
2023-04-26 19:27:10 +0800

    Update test.java

--- src/main/java/com/example/novel/config/test.java
+++ src/main/java/com/example/novel/config/test.java
@@ -2,12 +2,6 @@ package com.example.novel.config;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("hello 3");
-        System.out.println("hello master");
-        System.out.println("hello hot fix");
-        System.out.println("hot fix test");
-        System.out.println("idea push");
+        //删除此行
     }
 }

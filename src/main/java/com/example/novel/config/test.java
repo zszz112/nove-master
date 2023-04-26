59bbf72a2461253bca182e33ee7793a3513ab3f4
zszz <1652534360@qq.com>
2023-04-26 14:33:48 +0800

    third commit

--- src/main/java/com/example/novel/config/test.java
+++ src/main/java/com/example/novel/config/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
+        System.out.println("hello 3");
     }
 }
